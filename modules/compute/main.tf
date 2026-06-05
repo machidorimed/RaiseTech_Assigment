2d003ceb556fa12b8e0b9ed8a90734b2292453c7
machidori <Vernusylphgadjet@gmail.com>
2026-06-05 20:38:40 +0900

    ALB,WAF,Alarm削除

--- modules/compute/main.tf
+++ modules/compute/main.tf
@@ -1,92 +1,92 @@
-# ------------------------------------------------------------#
-#  ALB Security Group
-# ------------------------------------------------------------#
-resource "aws_security_group" "albsg" {
-  name        = "aws-study-${var.my_env}albsg"
-  description = "Allow HTTP access from internet"
-  vpc_id      = var.vpc_id
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+# # ------------------------------------------------------------#
+# #  ALB Security Group
+# # ------------------------------------------------------------#
+# resource "aws_security_group" "albsg" {
+#   name        = "aws-study-${var.my_env}albsg"
+#   description = "Allow HTTP access from internet"
+#   vpc_id      = var.vpc_id
+#   ingress {
+#     from_port   = 80
+#     to_port     = 80
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+#   egress {
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
 
-  tags = {
-    Name = "aws-study-${var.my_env}albsg"
-  }
-}
+#   tags = {
+#     Name = "aws-study-${var.my_env}albsg"
+#   }
+# }
 
-# ------------------------------------------------------------#
-#  ALB 
-# ------------------------------------------------------------#
-resource "aws_lb" "alb" {
-  name               = "aws-study-${var.my_env}alb"
-  internal           = false
-  load_balancer_type = "application"
-  security_groups    = [aws_security_group.albsg.id]
+# # ------------------------------------------------------------#
+# #  ALB 
+# # ------------------------------------------------------------#
+# resource "aws_lb" "alb" {
+#   name               = "aws-study-${var.my_env}alb"
+#   internal           = false
+#   load_balancer_type = "application"
+#   security_groups    = [aws_security_group.albsg.id]
 
-  subnets         = var.public_subnet_ids
-  ip_address_type = "ipv4"
+#   subnets         = var.public_subnet_ids
+#   ip_address_type = "ipv4"
 
-  tags = {
-    Name = "aws-study-${var.my_env}alb"
-  }
-}
+#   tags = {
+#     Name = "aws-study-${var.my_env}alb"
+#   }
+# }
 
-# ------------------------------------------------------------#
-#  Target Group
-# ------------------------------------------------------------#
-resource "aws_lb_target_group" "albtg" {
-  name             = "aws-study-${var.my_env}tg"
-  target_type      = "instance"
-  protocol_version = "HTTP1"
-  port             = 8080
-  protocol         = "HTTP"
+# # ------------------------------------------------------------#
+# #  Target Group
+# # ------------------------------------------------------------#
+# resource "aws_lb_target_group" "albtg" {
+#   name             = "aws-study-${var.my_env}tg"
+#   target_type      = "instance"
+#   protocol_version = "HTTP1"
+#   port             = 8080
+#   protocol         = "HTTP"
 
-  vpc_id = var.vpc_id
+#   vpc_id = var.vpc_id
 
-  tags = {
-    Name = "aws-study-${var.my_env}tg"
-  }
+#   tags = {
+#     Name = "aws-study-${var.my_env}tg"
+#   }
 
-  health_check {
-    interval            = 30
-    path                = "/"
-    port                = "traffic-port"
-    protocol            = "HTTP"
-    timeout             = 5
-    healthy_threshold   = 5
-    unhealthy_threshold = 2
-    matcher             = "200,300,301"
-  }
-}
+#   health_check {
+#     interval            = 30
+#     path                = "/"
+#     port                = "traffic-port"
+#     protocol            = "HTTP"
+#     timeout             = 5
+#     healthy_threshold   = 5
+#     unhealthy_threshold = 2
+#     matcher             = "200,300,301"
+#   }
+# }
 
-resource "aws_lb_target_group_attachment" "test_target_ec2" {
-  target_group_arn = aws_lb_target_group.albtg.arn
-  target_id        = aws_instance.ec2.id
-}
+# resource "aws_lb_target_group_attachment" "test_target_ec2" {
+#   target_group_arn = aws_lb_target_group.albtg.arn
+#   target_id        = aws_instance.ec2.id
+# }
 
-# ------------------------------------------------------------#
-#  ALB Listner
-# ------------------------------------------------------------#
-resource "aws_lb_listener" "alb_listener" {
-  load_balancer_arn = aws_lb.alb.arn
-  port              = "80"
-  protocol          = "HTTP"
+# # ------------------------------------------------------------#
+# #  ALB Listner
+# # ------------------------------------------------------------#
+# resource "aws_lb_listener" "alb_listener" {
+#   load_balancer_arn = aws_lb.alb.arn
+#   port              = "80"
+#   protocol          = "HTTP"
 
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.albtg.arn
-  }
-}
+#   default_action {
+#     type             = "forward"
+#     target_group_arn = aws_lb_target_group.albtg.arn
+#   }
+# }
 
 # ------------------------------------------------------------#
 #  EC2 Security Group
@@ -97,10 +97,10 @@ resource "aws_security_group" "ec2_sg" {
 
   # App only access from alb
   ingress {
-    from_port       = 8080
-    to_port         = 8080
-    protocol        = "tcp"
-    security_groups = [aws_security_group.albsg.id]
+    from_port = 8080
+    to_port   = 8080
+    protocol  = "tcp"
+    #    security_groups = [aws_security_group.albsg.id]
   }
 
   egress {
